7db77eaca7977902ef96d8ba79370ce4d8061088
cedlemo <cedlemo@gmx.com>
2019-10-06 14:18:37 +0200

    Add headerbar

--- src/tortosa.c
+++ src/tortosa.c
@@ -6,10 +6,15 @@ activate (GtkApplication *app,
           gpointer user_data)
 {
     GtkWidget *window;
+    GtkWidget *headerbar;
 
     window = gtk_application_window_new (app);
     gtk_window_set_title (GTK_WINDOW (window), "Window");
     gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+    headerbar = gtk_header_bar_new();
+   gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(headerbar), TRUE);
+    gtk_window_set_titlebar(GTK_WINDOW(window), headerbar);
     gtk_widget_show_all (window);
 }
 

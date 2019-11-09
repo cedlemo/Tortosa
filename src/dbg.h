d8029306d33d057ae0f46e7af4b4ee625304fb7c
cedlemo <cedlemo@gmx.com>
2019-11-09 19:01:45 +0100

    Add license information

--- src/dbg.h
+++ src/dbg.h
@@ -1,10 +1,21 @@
-/**
- * \file dbg.h
- * \brief Define dbg helpers
- * \author Cedlemo
- * \version 0.1
- * \date 18 mai 2013
+/*
+ * Copyright 2019 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of Tortosa Terminal Emulator.
+ *
+ * Tortosa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Tortosa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tortosa.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
--- src/terminal.c
+++ src/terminal.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2019 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of Tortosa Terminal Emulator.
+ *
+ * Tortosa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Tortosa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tortosa.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "terminal.h"
 #include "shell.h"
 #include "dbg.h"

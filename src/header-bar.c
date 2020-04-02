56971a8093ae4759d2a3f865b63fa2dd0c45c64e
cedlemo <cedlemo@gmx.com>
2020-04-02 19:18:20 +0200

    HeaderBar: add custom buttons for prev/next tab

--- src/header-bar.c
+++ src/header-bar.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Cedric LE MOIGNE, cedlemo@gmx.com
+ * Copyright 2019-2020 Cedric LE MOIGNE, cedlemo@gmx.com
  * This file is part of Tortosa Terminal Emulator.
  *
  * Tortosa is free software: you can redistribute it and/or modify
@@ -17,7 +17,9 @@
  */
 
 #include "header-bar.h"
+#include "event-label.h"
 #include "shell.h"
+#include "notebook.h"
 
 struct _TortosaHeaderBar {
     GtkHeaderBar parent_instance;
@@ -25,12 +27,48 @@ struct _TortosaHeaderBar {
 
 G_DEFINE_TYPE (TortosaHeaderBar, tortosa_header_bar, GTK_TYPE_HEADER_BAR)
 
+static gboolean
+click_prev (GtkWidget *event_label, GdkEvent *event, gpointer data)
+{
+    GList *children = gtk_container_get_children (GTK_CONTAINER (event_label));
+    GtkLabel *label = GTK_LABEL ((g_list_first (children)->data));
+    g_list_free (children);
+
+    g_message ("hit %s", gtk_label_get_text (label));
+    TortosaNotebook *notebook = tortosa_shell_get_notebook ();
+    tortosa_notebook_prev (notebook);
+
+    return TRUE;
+}
+
+static gboolean
+click_next (GtkWidget *event_label, GdkEvent *event, gpointer data)
+{
+    GList *children = gtk_container_get_children (GTK_CONTAINER (event_label));
+    GtkLabel *label = GTK_LABEL ((g_list_first (children)->data));
+    g_list_free (children);
+
+    g_message ("hit %s", gtk_label_get_text (label));
+
+    TortosaNotebook *notebook = tortosa_shell_get_notebook ();
+    tortosa_notebook_prev (notebook);
+
+    return TRUE;
+}
 static void
 tortosa_header_bar_init (TortosaHeaderBar *header_bar)
 {
     gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(header_bar), TRUE);
     gtk_widget_set_name (GTK_WIDGET (header_bar), "tortosa-headerbar");
+
+    TortosaEventLabel *prev_label = tortosa_event_label_new ("<");
+    tortosa_event_label_set_button_press_cb (prev_label, click_prev);
+    TortosaEventLabel *next_label = tortosa_event_label_new (">");
+    tortosa_event_label_set_button_press_cb (prev_label, click_next);
+
+    gtk_header_bar_pack_start (GTK_HEADER_BAR (header_bar), GTK_WIDGET (prev_label));
     gtk_header_bar_pack_start (GTK_HEADER_BAR (header_bar), GTK_WIDGET (tortosa_shell_get_term_title ()));
+    gtk_header_bar_pack_start (GTK_HEADER_BAR (header_bar), GTK_WIDGET (next_label));
 }
 
 static void
